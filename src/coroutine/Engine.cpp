6167e404137c5b46c8cf66faba2ef661671c6b0a
AnastasiiaNovikova <nastuxa8805@mail.ru>
2017-12-28 00:11:32 +0300

    Add files via upload

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -1,70 +1,90 @@
 #include <afina/coroutine/Engine.h>
 
 #include <setjmp.h>
-#include <string.h>
 #include <stdio.h>
 #include <string.h>
 
-
 namespace Afina {
-namespace Coroutine {
-
-void Engine::Store(context &ctx) {
-    char ch;
-    char* t = &ch;
-    
-    std::ptrdiff_t len =  StackBottom - t;
-    delete [](std::get<0>(ctx.Stack));
-    
-    std::get<0>(ctx.Stack) = new char[len]; 
-    std::get<1>(ctx.Stack)  = len;
-    
-    memcpy(std::get<0>(ctx.Stack), t, len);
-
-}
-    
-
-void Engine::Restore(context &ctx) {
-    volatile char ch;
-    char* t = (char*)&ch;
-    
-    if((char*)&ch > StackBottom - std::get<1>(ctx.Stack)){
-            Restore(ctx); 
+    namespace Coroutine {
+        void Engine::Store(context &ctx) {
+            char stackStart;
+            //Должен сохранить регистры и стэк
+            ctx.Low = ctx.Height = this->StackBottom;
+            if (&stackStart>ctx.Low){
+                ctx.Height = &stackStart;
+            }
+            else{
+                ctx.Low = &stackStart;
+            }
+            int size = ctx.Height - ctx.Low;
+            
+            // If dst and src overlap, behavior is undefined. To avoid this, we make:
+            if (std::get<1>(ctx.Stack)<size){
+                delete std::get<0>(ctx.Stack);
+                std::get<0>(ctx.Stack) = new char[size];
+                std::get<1>(ctx.Stack) = size;
+            }
+            
+            memcpy(std::get<0>(ctx.Stack), ctx.Low, size);
         }
-    char* top = StackBottom - std::get<1>(ctx.Stack);
-    
-    memcpy(top, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
-    longjmp(ctx.Environment, 1);
-
-}
-    
-
-void Engine::yield() {
-    if (alive){
-        context * tmp = alive;
-        alive -> prev = nullptr;
-        alive = alive -> next;
-        sched(tmp);
-    }
-
-}
-    
-
-void Engine::sched(void *routine_) {
-    context * ctx = (context*)routine_; 
-    ctx->caller = cur_routine;
-    
-    if (cur_routine != nullptr){ 
-        cur_routine->callee = ctx;  
-        Store(*cur_routine);
         
-        if (setjmp(cur_routine -> Environment) !=0){ 
-            return;
+        void Engine::Restore(context &ctx) {
+            char stackStart;
+            char *StackAddr = &stackStart;
+            if(ctx.Low<=StackAddr && StackAddr<=ctx.Height) Restore(ctx);
+            
+            // memcpy(void *restrict dst, const void *restrict src, size_t n);
+            // The memcpy() function copies n bytes from memory area src to memory area dst.
+            memcpy(ctx.Low, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
+            longjmp(ctx.Environment, 1);
         }
-    }
-    cur_routine = ctx;
-    Restore(*ctx);
-}
-
-} 
-} 
+        
+        void Engine::yield() {
+            if (cur_routine == nullptr and alive == nullptr){
+                return;
+            }
+            
+            context* to_call = nullptr;
+            for (context* p = alive; p != nullptr; p = p->next) {
+                if (p != cur_routine) { // find any routine != cur_routine
+                    to_call = p;
+                    break;
+                }
+            }
+            
+            sched(to_call);
+        }
+        
+        
+        void Engine::sched(void *routine_) {
+            context *to_call = (context *)routine_;
+            
+            if (to_call == cur_routine) {
+                return;
+            }
+            
+            
+            // these lines are required to exit last run() correctly
+            if (to_call == nullptr) {
+                if (alive == nullptr) {
+                    return;   // no coroutines remain
+                }
+                else {
+                    to_call = alive;
+                }
+            }
+            
+            if (cur_routine){
+                Store(*cur_routine); //so there would be a place to return to
+                if (setjmp(cur_routine -> Environment)){
+                    return;
+                }
+            }
+            // throw current frame away
+            cur_routine = to_call;
+            Restore(*to_call);
+            
+        }
+        
+    } // namespace Coroutine
+} // namespace Afina
